76ae5a9a6331a3367543f197f0cd5c842c8fb686
MaximeRoussiere <maximo85.mr@gmail.com>
2020-03-04 21:14:00 +0100

    full-integration

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,34 @@
+var Rellax = require('rellax');
+
 var app = {
   init: function() {
     console.log('init');
 
+    // PRELOADER
+
+    $(window).on('load', function() { // makes sure the whole site is loaded 
+      $('.preloader').fadeOut(6000); // will first fade out the loading animation 
+      $('.loader').delay(5000).fadeOut('fast'); // will fade out the white DIV that covers the website. 
+      $('body').delay(4000).css({'overflow-y':'visible'});
+    })
+
+    // PARALLAX BANNER
+
+    var rellax = new Rellax('.rellax');
+
+    // HEADER BECOME TRANSPARENT
+
+    $(document).ready(function(){
+      $(window).scroll(function(){
+        $('.banner__header').css("opacity", 1 - $(window).scrollTop() /375)
+      })
+    })
+
+    $(document).ready(function(){
+      $(window).scroll(function(){
+        $('.tree_img').css("opacity", 1 - $(window).scrollTop() /475)
+      })
+    })
     // ANIME HEADER NAVBAR SCROLL
 
     $(window).on('scroll', function(){
@@ -14,6 +41,8 @@ var app = {
       $('nav').removeClass('black');
     }
   })
+
+  
 
     // ANIME HEADER NAVABAR RESPONSIVE ONCLICK
 
@@ -35,7 +64,7 @@ var app = {
 	$(document).on('scroll', onScroll);  
 	
 	$("body").on('click', '[href*="#"]', function(e){  
-	  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top -150}, 900);
+	  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top -150}, 100);
 	  e.preventDefault();
 	});    
 	  
@@ -54,9 +83,22 @@ var app = {
 					currentLink.removeClass("nav__link--active");
 				}
 			});
-		}
+    }
+    
+  // BACK TO TOP BUTTON
 
-	// Input Lock
+  // back to top function
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$("#home").offset().top},"200");return false})})
+
+	// CONTACT FORM Input Lock
 $('textarea').blur(function () {
     $('#hire textarea').each(function () {
         $this = $(this);
@@ -98,7 +140,44 @@ $('#hire .field:nth-child(2) input').blur(function () {
         }
     });
 });
-  
+
+//ANIMATE
+
+var $animation_elements = $('.sec2_content');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('animated fadeInLeft duration-4s delay-0.5s');
+    } else {
+      $element.removeClass('animated fadeInLeft duration-4s delay-0.5s');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+
+//
+
+
+
+
+
+
   }
 };
 
